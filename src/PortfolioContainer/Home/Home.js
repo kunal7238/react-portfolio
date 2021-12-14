b53846ec932a68a34b0e1e7e081c1b035c8eb9d7
kunal7238 <kunal.singh_cs19@gla.ac.in>
2021-12-14 22:05:42 +0530

    Home Component complete

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Profile from './Profile';
 import Footer from './Footer/Footer';
 import './Home.css';
-
 export default function Home() {
     return (
-        <div className='home-conatiner'>
+        <div className='home-container'>
             <Profile/>
             <Footer/>
         </div>
