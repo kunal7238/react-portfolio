a25e4404345043b86bdce6ae2b018c41ebb80439
kunal7238 <kunal.singh_cs19@gla.ac.in>
2021-12-17 01:19:11 +0530

    about section responsive cmplete

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -31,7 +31,7 @@ export default function AboutMe(props) {
      const renderHighlight = () => {
          return (
              SCREEN_CONSTANTS.highlights.bullets.map((value, i) =>(
-                    <div className="highlight" >
+                    <div className="highlight" key={i}>
                         <div className="highlight-blob"></div>
                         <span>{value}</span>
                     </div>
@@ -41,31 +41,24 @@ export default function AboutMe(props) {
     return (
         <div className="about-me-container screen-container" id={props.id || ""}>
             <div className="about-me-parent">
-
                 <ScreenHeading title={"About Me"} subHeading={'Why Shoose Me ?'}/>
-
                 <div className="about-me-card">
-                    
-                    <div className="about-me-profile">
+                    <div className="about-me-profile"></div>
                         <div className="about-me-details">
                             <span className="about-me-description">{SCREEN_CONSTANTS.description}</span>
-
                             <div className="about-me-highlights">
                                 <div className="highlight-heading">
                                     <span>{SCREEN_CONSTANTS.highlights.heading}</span>
                                 </div>
                                 {renderHighlight()}
                             </div>
-
                             <div className="about-me-options">
                                 <button className="btn primary-btn">Hire Me</button>
                                 <a href="kunal.pdf" download="kunal.pdf">
                                     <button className="btn highlighted-btn">Get Resume</button>
                                 </a> 
                             </div>
-
                         </div>
-                    </div>
                 </div>
             </div>         
         </div>
